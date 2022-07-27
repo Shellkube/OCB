bd4da4623ac360772ca344bcedf2850cda368f0b
KubeAl <96569040+KubeAl@users.noreply.github.com>
2022-07-27 12:30:19 +0530

    standard storage class

--- addons/analytic/__manifest__.py
+++ addons/analytic/__manifest__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name' : 'Analytic Accounting-minikube',
+    'name' : 'Analytic Accounting-standard',
     'version': '1.1',
     'category': 'Accounting/Accounting',
     'depends' : ['base', 'mail', 'uom'],
