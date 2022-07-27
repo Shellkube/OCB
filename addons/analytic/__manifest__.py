7b4160d37162f40d8fae9abee3e10fb971ff186e
KubeAl <96569040+KubeAl@users.noreply.github.com>
2022-07-27 12:14:55 +0530

    no finalizer in pvc

--- addons/analytic/__manifest__.py
+++ addons/analytic/__manifest__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name' : 'Analytic Accounting-check11',
+    'name' : 'Analytic Accounting-check12',
     'version': '1.1',
     'category': 'Accounting/Accounting',
     'depends' : ['base', 'mail', 'uom'],
