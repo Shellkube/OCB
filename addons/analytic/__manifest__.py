c3936e59476a7332d637b45ec0e9a990eb31f776
KubeAl <96569040+KubeAl@users.noreply.github.com>
2022-07-27 12:52:59 +0530

    back to normal

--- addons/analytic/__manifest__.py
+++ addons/analytic/__manifest__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name' : 'Analytic Accounting-standard',
+    'name' : 'Analytic Accounting',
     'version': '1.1',
     'category': 'Accounting/Accounting',
     'depends' : ['base', 'mail', 'uom'],
