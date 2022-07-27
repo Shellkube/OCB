e447713831894150f60aaab8fe4a4c914d67ca58
KubeAl <96569040+KubeAl@users.noreply.github.com>
2022-07-27 11:08:39 +0530

    case sensitive in repo url

--- addons/analytic/__manifest__.py
+++ addons/analytic/__manifest__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name' : 'Analytic Accounting-check2',
+    'name' : 'Analytic Accounting-check3',
     'version': '1.1',
     'category': 'Accounting/Accounting',
     'depends' : ['base', 'mail', 'uom'],
